aa10c6889238b728003a868c6d1d77fb4c8b2878
Kang <rkdalsgur032@unist.ac.kr>
2023-05-29 22:25:04 +0900

    chore : initial setting with ios-swift

---
+++ Mix.io/Mix.io/Mix_ioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mix_ioApp.swift
+//  Mix.io
+//
+//  Created by 강민혁 on 2023/05/29.
+//
+
+import SwiftUI
+
+@main
+struct Mix_ioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mix.io/Mix.ioUITests/Mix_ioUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Mix_ioUITestsLaunchTests.swift
+//  Mix.ioUITests
+//
+//  Created by 강민혁 on 2023/05/29.
+//
+
+import XCTest
+
+final class Mix_ioUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
